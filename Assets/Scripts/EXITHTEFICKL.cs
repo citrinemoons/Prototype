7a296989d2a0b9750652379a59b390c74487f08a
TheLastSlice <56330886+TheLastSlice898@users.noreply.github.com>
2023-12-14 10:38:37 +1100

    Expo Build

---
+++ Assets/Scripts/EXITHTEFICKL.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EXITHTEFICKL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void EXITGO()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
+}
